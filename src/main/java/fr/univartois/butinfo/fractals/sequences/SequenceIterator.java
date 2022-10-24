6088d8d09ebeb2558dd2d625a8e0d49dadd74ab3
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-24 16:23:39 +0200

    Ajout de la documentation de la classe SequenceIterator.

--- src/main/java/fr/univartois/butinfo/fractals/sequences/SequenceIterator.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/SequenceIterator.java
@@ -4,28 +4,41 @@ import java.util.Iterator;
 
 import fr.univartois.butinfo.fractals.complex.IComplex;
 
+/**
+ * La classe SequenceIterator correspond à l'Iterator de la suite (classe
+ * Sequence).
+ *
+ * @author Sébastien Coze
+ *
+ * @version 0.1.0
+ */
 public class SequenceIterator implements Iterator<IComplex> {
-	
-	private IComplex presentTerm;
-	
-	private INextTerm nextTerm;
-	
+
+	/**
+	 * La suite de nombres complexes.
+	 */
 	private Sequence sequence;
 
+	/**
+	 * Crée une nouvelle instance de Sequence.
+	 * 
+	 * @param sequence La suite de nombres complexes.
+	 */
 	public SequenceIterator(Sequence sequence) {
 		this.sequence = sequence;
 	}
 
 	@Override
 	public boolean hasNext() {
-		return (sequence.getPresentTerm().abs() >= 2) || (sequence.getNextTerm().calculateNextTerm(sequence.getPresentTerm()) != null);
+		return (sequence.getPresentTerm().abs() >= 2)
+				|| (sequence.getNextTerm().calculateNextTerm(sequence.getPresentTerm()) != null);
 	}
 
 	@Override
 	public IComplex next() {
 		if (hasNext()) {
-			presentTerm = nextTerm.calculateNextTerm(presentTerm);
-			return presentTerm;	
+			sequence.setPresentTerm(sequence.getNextTerm().calculateNextTerm(sequence.getPresentTerm()));
+			return sequence.getPresentTerm();
 		}
 		return null;
 	}
