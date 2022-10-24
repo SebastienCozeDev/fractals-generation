d5d2d98c6e1f92cbc0d7685f2496e5b8c71739ee
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-24 15:20:13 +0200

    Ajout de la classe PaletteColor.java

---
+++ src/main/java/fr/univartois/butinfo/fractals/color/PaletteColorDecorateur.java
@@ -0,0 +1,13 @@
+package fr.univartois.butinfo.fractals.color;
+
+import java.awt.Color;
+
+public class PaletteColorDecorateur implements IPaletteColor {	
+
+	@Override
+	public void paletteColor(Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
