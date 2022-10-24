48f25006f623da9b54e252dcd0119b1e5ddaca45
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-24 12:49:02 +0200

    Ajout de l'interface INextTerm définissant le contrat à respecter par la strategy de la méthode permettant de calculler le prochain terme d'une suite.

---
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/INextTerm.java
@@ -0,0 +1,19 @@
+package fr.univartois.butinfo.fractals.sequences;
+
+/**
+ * L'interface INextTerm définit le contrat à respecter par la strategy de la
+ * méthode calculateNextTerm() permettant de calculer le prochain terme d'une
+ * suite.
+ *
+ * @author Sébastien Coze
+ *
+ * @version 0.1.0
+ */
+public interface INextTerm {
+
+	/**
+	 * Permet de calculer le prochain terme d'une suite.
+	 */
+	void calculateNextTerm();
+
+}
