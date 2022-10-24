7cebb36f8f943602eb92050180f164e7d1628033
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-24 12:56:33 +0200

    Ajout de l'interface IColor.java pour representer les differentes palettes de couleurs

---
+++ src/main/java/fr/univartois/butinfo/fractals/color/IColor.java
@@ -0,0 +1,19 @@
+package fr.univartois.butinfo.fractals.color;
+
+import java.awt.Color;
+
+/**
+ * L'interface donne la methode qu'on va utilisé pour notre palette de couleur.
+ *
+ * @author Theo Journee
+ *
+ * @version 0.1.0
+ */
+public interface IColor {
+	
+	/**
+	 * 
+	 * @param color
+	 */
+	void Color(Color color);
+}
