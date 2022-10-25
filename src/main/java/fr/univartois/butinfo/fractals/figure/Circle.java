a242c8f55ec4285f0579ce4a936abd4bb99e5f0e
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 16:33:41 +0200

    Ajout du ToString et constructeur

--- src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
@@ -3,24 +3,18 @@ package fr.univartois.butinfo.fractals.figure;
 public class Circle {
 	
 	/**
-	 * Largeur du rectangle.
+	 * rayon du cercle.
 	 */
-	private int width;
-
-	/**
-	 * Hauteur du rectangle.
-	 */
-	private int height;
-
+	private int r;
 	/**
 	 * position x corne gauche du rectangle.
 	 */
-	private int rx;
+	private int cx;
 
 	/**
 	 * position y corne gauche du rectangle.
 	 */
-	private int ry;
+	private int cy;
 
 	/**
 	 * Couleur du rectangle.
@@ -37,5 +31,19 @@ public class Circle {
 	 */
 	private int strokeWidth;
 
-
+	public Circle(int r, int cx, int cy, String stroke, String fill, int strokeWidth) {
+		this.r=r;
+		this.cx = cx;
+		this.cy = cy;
+		this.stroke = stroke;
+		this.fill = fill;
+		this.strokeWidth = strokeWidth;
+	}
+	/**
+	 * Methode de chaine de caractere svg.
+	 */
+	public String toString() {
+		return "<svg width=\"200\" height=\"250\" version=\"1.1\"> < circle  cx=" + cx + ", cy=" + cy+",r=" + r
+				+ ", stroke=" + stroke + ", fill=" + fill + ", strokeWidth=" + strokeWidth + "/> </svg>";
+	}
 }
