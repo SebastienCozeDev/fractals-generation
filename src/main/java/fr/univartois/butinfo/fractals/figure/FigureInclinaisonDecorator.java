dd7fb55ad1270c2adfdeb9d6a80b39ca64b7633a
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 20:05:14 +0200

    Ajout commentaire

--- src/main/java/fr/univartois/butinfo/fractals/figure/FigureInclinaisonDecorator.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/FigureInclinaisonDecorator.java
@@ -1,5 +1,12 @@
 package fr.univartois.butinfo.fractals.figure;
 
+/**
+ * Le decorateur pour ajouter une inclinaison.
+ *
+ * @author Theo Journee
+ *
+ * @version 0.1.0
+ */
 public class FigureInclinaisonDecorator implements IFigure {
 
 	/**
