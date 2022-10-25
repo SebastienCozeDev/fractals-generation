50eb9cc1cfdd64c41704d2f8bbaa89e6960861ef
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-25 13:54:02 +0200

    Fin de question 4

--- src/main/java/fr/univartois/butinfo/fractals/complex/ComplexImage.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/ComplexImage.java
@@ -1,21 +1,17 @@
 package fr.univartois.butinfo.fractals.complex;
 
-public  class ComplexImage {
+public class ComplexImage  {
 	
-	private final double re;   // the real part
-	private final double im;   // the imaginary part
-	
+	private IComplexImage decorated;
+	private final double re; // the real part
+	private final double im; // the imaginary part
+
 	private int height;
 	private int width;
-	
-	
-	
-	
+
 	public int getHeight() {
 		return height;
 	}
-
-
 
 	public int getWidth() {
 		return width;
@@ -25,40 +21,22 @@ public  class ComplexImage {
 		return re;
 	}
 
-
 	public double getIm() {
 		return im;
 	}
 
-
 	public ComplexImage(double re, double im, int height, int width) {
-		super();
 		this.re = re;
 		this.im = im;
 		this.height = height;
 		this.width = width;
 	}
 
-	
 	public IComplex asComplex(int row, int column) {
-		 double re = (column + .5) - (width / 2.);
-		 double im = (height / 2.) - (row + .5);
-		 return new Complex(re, im);
-		}
-
-
-	
-	
-	
-	
-
-
-
-
-
-
-
-
-	
-	
+		double re = (column + .5) - (width / 2.);
+		double im = (height / 2.) - (row + .5);
+		return decorated.asComplex(new Complex(re,im));
+		
 	}
+
+}
