30b9bafff84ab6eea23d60fec9c2600ffa04ffa9
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-25 16:55:17 +0200

    Creation de l'interface IplanPoint pour l'adaptateur

---
+++ src/main/java/fr/univartois/butinfo/fractals/complex/IPlanPoint.java
@@ -0,0 +1,9 @@
+package fr.univartois.butinfo.fractals.complex;
+
+public interface IPlanPoint {
+	  public double X();
+	  public double Y();
+	  public double distance(IPlanPoint point);
+	   
+
+}
