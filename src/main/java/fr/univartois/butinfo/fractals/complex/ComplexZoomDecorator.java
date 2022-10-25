5e933495f1f91081b6944791fcbc595d7f013c60
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-25 14:19:12 +0200

    Documentation de la classe ComplexZoomDecorator

--- src/main/java/fr/univartois/butinfo/fractals/complex/ComplexZoomDecorator.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/ComplexZoomDecorator.java
@@ -1,15 +1,15 @@
 package fr.univartois.butinfo.fractals.complex;
 
+/*decorateur permettant de zoomer sur l'image*/
+
 public abstract class ComplexZoomDecorator implements IComplexImage {
-	
-	
-	
+
 	private final double constant;
 
 	public ComplexZoomDecorator(double constant) {
 		this.constant = constant;
 	}
-	
+
 	@Override
 	public IComplex asComplex(IComplex complex) {
 		return complex.multiply(constant);
