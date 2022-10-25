549925216f13acc99a16cf9c1df8491baaca3a07
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 15:55:15 +0200

    Ajout classe Circle

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
@@ -0,0 +1,40 @@
+package fr.univartois.butinfo.fractals.figure;
+
+public class Circle {
+	
+	/**
+	 * Largeur du rectangle.
+	 */
+	private int width;
+
+	/**
+	 * Hauteur du rectangle.
+	 */
+	private int height;
+
+	/**
+	 * position x corne gauche du rectangle.
+	 */
+	private int rx;
+
+	/**
+	 * position y corne gauche du rectangle.
+	 */
+	private int ry;
+
+	/**
+	 * Couleur du rectangle.
+	 */
+	private String stroke;
+
+	/**
+	 * le fond.
+	 */
+	private String fill;
+
+	/**
+	 * largeur de couleur.
+	 */
+	private int strokeWidth;
+
+}
