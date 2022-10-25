9fda8e59db2cfcf2b6b50353918f0a7363dbaefb
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-25 16:56:52 +0200

    Création de la classe AdaptateurComplex

---
+++ src/main/java/fr/univartois/butinfo/fractals/complex/AdaptateurComplex.java
@@ -0,0 +1,23 @@
+package fr.univartois.butinfo.fractals.complex;
+
+public class AdaptateurComplex implements IPlanPoint {
+
+	@Override
+	public double X() {
+		
+		return 0;
+	}
+
+	@Override
+	public double Y() {
+		
+		return 0;
+	}
+
+	@Override
+	public double distance(IPlanPoint point) {
+		
+		return 0;
+	}
+
+}
