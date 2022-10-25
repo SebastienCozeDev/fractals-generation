dabdd6781ae81c355d38c38cada8d11c76e10fe2
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 14:34:13 +0200

    Ajout du package figure

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Le paquetage {@code fr.univartois.butinfo.fractals.figure} fournit les classes
+ * permettant de manipuler les figures.
+ *
+ * @author Theo Journee
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.fractals.figure;
