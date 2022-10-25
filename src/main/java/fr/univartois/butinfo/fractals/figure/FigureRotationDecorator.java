87f693265ee4868cce0e1563f77c915b27cdf401
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 20:05:29 +0200

    Ajout commentaire

--- src/main/java/fr/univartois/butinfo/fractals/figure/FigureRotationDecorator.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/FigureRotationDecorator.java
@@ -1,5 +1,12 @@
 package fr.univartois.butinfo.fractals.figure;
 
+/**
+ * Le decorateur pour ajouter une rotation.
+ *
+ * @author Theo Journee
+ *
+ * @version 0.1.0
+ */
 public class FigureRotationDecorator implements IFigure {
 
 	/**
