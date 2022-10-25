3c71d542336f0c42d58a2e7f1dfdfe747f93c317
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 17:01:19 +0200

    Ajout des methodes

--- src/main/java/fr/univartois/butinfo/fractals/figure/Ellipse.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Ellipse.java
@@ -1,5 +1,54 @@
 package fr.univartois.butinfo.fractals.figure;
 
 public class Ellipse {
+	
+	/**
+	 * rayon x.
+	 */
+	private int rx;
 
+	/**
+	 * rayon y.
+	 */
+	private int ry;
+
+	/**
+	 * position x corne gauche du rectangle.
+	 */
+	private int cx;
+
+	/**
+	 * position y corne gauche du rectangle.
+	 */
+	private int cy;
+
+	/**
+	 * Couleur du rectangle.
+	 */
+	private String stroke;
+
+	/**
+	 * le fond.
+	 */
+	private String fill;
+
+	/**
+	 * largeur de couleur.
+	 */
+	private int strokeWidth;
+
+	public Ellipse( int cx, int cy, String stroke, String fill, int strokeWidth) {
+		this.cx = cx;
+		this.cy = cy;
+		this.stroke = stroke;
+		this.fill = fill;
+		this.strokeWidth = strokeWidth;
+	}
+	/**
+	 * Methode de chaine de caractere svg.
+	 */
+	public String toString() {
+		return "<svg width=\"200\" height=\"250\" version=\"1.1\"> < circle  cx=" + cx + ", cy=" + cy + ",rx=" + rx + ", ry=" + ry 
+				+ ", stroke=" + stroke + ", fill=" + fill + ", strokeWidth=" + strokeWidth + "/> </svg>";
+	}
 }
