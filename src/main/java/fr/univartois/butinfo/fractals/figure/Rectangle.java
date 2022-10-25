b688b5ad2109fd7ee73739dd0d4d435bad57b72c
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 15:08:03 +0200

    Ajout des atributs pour le rectangle et le constructeur

--- src/main/java/fr/univartois/butinfo/fractals/figure/Rectangle.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Rectangle.java
@@ -1,5 +1,52 @@
 package fr.univartois.butinfo.fractals.figure;
 
 public class Rectangle {
+	
+	/**
+	 * position x coté gauche du rectangle.
+	 */
+	private int x;
+	
+	/**
+	 * position y coté gauche du rectangle
+	 */
+	private int y;
+	
+	/**
+	 * Largeur du rectangle.
+	 */
+	private int width;
+	
+	/**
+	 * Hauteur du rectangle.
+	 */
+	private int height;
+	
+	/**
+	 * position x corne gauche du rectangle.
+	 */
+	private int rx;
+	
+	/**
+	 * position y corne gauche du rectangle.
+	 */
+	private int ry;
 
+	/**
+	 * Constructeur
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 * @param rx
+	 * @param ry
+	 */
+	public Rectangle(int x, int y, int width, int height, int rx, int ry) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.rx = rx;
+		this.ry = ry;
+	}
 }
