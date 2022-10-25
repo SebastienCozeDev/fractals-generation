8f54c843b38773f9cbb109ba1a828cfdf897c551
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-25 09:10:54 +0200

    Ajout de la stratégie (strategy) MandelbrotNextTerm pour calculer le prochain terme de la suite de Mandelbrot.

---
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/MandelbrotNextTerm.java
@@ -0,0 +1,33 @@
+package fr.univartois.butinfo.fractals.sequences;
+
+import fr.univartois.butinfo.fractals.complex.IComplex;
+
+/**
+ * La classe {@link MandelbrotNextTerm} correspond à la stratégie (strategy) pour calculer le
+ * prochain terme de la suite de Mandelbrot.
+ *
+ * @author Sébastien Coze
+ *
+ * @version 0.1.0
+ */
+public class MandelbrotNextTerm implements INextTerm {
+	
+	@Override
+	public void setFirstTerm(IComplex firstTerme) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPresentTerm(IComplex presentTerm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IComplex calculateNextTerm(IComplex lastTerm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
