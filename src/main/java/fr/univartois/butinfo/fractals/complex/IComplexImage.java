64d7cf07aedbd2d5e9e798d5af93abf19413ef55
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-25 14:19:51 +0200

    Documentation de l'interface IComplexImage

--- src/main/java/fr/univartois/butinfo/fractals/complex/IComplexImage.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/IComplexImage.java
@@ -1,7 +1,8 @@
 package fr.univartois.butinfo.fractals.complex;
 
+/*L'interface IcomplexImage correspond au contrat de commplexImage */
+
 public interface IComplexImage {
-	
 	IComplex asComplex(IComplex complex);
 
 }
