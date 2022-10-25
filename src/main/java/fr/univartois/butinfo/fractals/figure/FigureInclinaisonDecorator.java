74aac365fc496f234bf8edcda4cc8fa13ddf0a04
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 19:31:42 +0200

    Ajout decorateur et methode inclinaison

--- src/main/java/fr/univartois/butinfo/fractals/figure/FigureInclinaisonDecorator.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/FigureInclinaisonDecorator.java
@@ -1,5 +1,15 @@
 package fr.univartois.butinfo.fractals.figure;
 
-public class FigureInclinaisonDecorator {
+public class FigureInclinaisonDecorator implements IFigure {
+
+	/**
+	 * Angle x qui détermine dans quelle mesure l'élément sera incliné.
+	 */
+	private int x;
+	
+	@Override
+	public String ToString() {
+		return "transform= skewX("+x+")";
+	}
 
 }
