6297388d6e49370c4f0e14c73b67503bd0bc5bda
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 19:57:32 +0200

    Ajout du polyligne

--- src/main/java/fr/univartois/butinfo/fractals/figure/Polyligne.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Polyligne.java
@@ -2,4 +2,32 @@ package fr.univartois.butinfo.fractals.figure;
 
 public class Polyligne {
 
+	/**
+	 * position.
+	 */
+	private int x;
+
+	/**
+	 * Couleur du polyligne.
+	 */
+	private String stroke;
+
+	/**
+	 * largeur.
+	 */
+	private int strokeWidth;
+	
+	private IFigure decorated;
+	
+	public Polyligne(String stroke) {
+		this.stroke = stroke;
+	}
+
+	/**
+	 * Methode de chaine de caractere svg.
+	 */
+	public String toString() {
+		return "<svg width=\"200\" height=\"250\" version=\"1.1\"> < points=\"50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180\""
+				+ ", stroke=" + stroke + ", strokeWidth=" + strokeWidth +  decorated + "/> </svg>";
+	}
 }
