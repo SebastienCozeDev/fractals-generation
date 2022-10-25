e5d2507889f32f7de6d55770443a9cbb743b9f1d
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-25 10:53:35 +0200

    Ajout de la methode saveAs

--- src/main/java/fr/univartois/butinfo/fractals/image/AdaptateurBufferedImage.java
+++ src/main/java/fr/univartois/butinfo/fractals/image/AdaptateurBufferedImage.java
@@ -2,23 +2,40 @@ package fr.univartois.butinfo.fractals.image;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * La classe Adaptateur de BuffedImage pour implementer IFractalImage.
+ * 
  * @author Theo Journee
  *
  * @version 0.1.0
  */
 public class AdaptateurBufferedImage implements IFractalImage {
 
-	
+	/**
+	 * Instance de la classe BifferedImage.
+	 */
 	private final BufferedImage bufferedimage;
+
+	/**
+	 * Instance de la la classe Pixel.
+	 */
 	private Pixel pixel;
-	
-	public AdaptateurBufferedImage(BufferedImage bufferedimage,IFractalImage image) {
-		this.bufferedimage=bufferedimage;	
+
+	/**
+	 * Constructeur de la classe
+	 * 
+	 * @param bufferedimage
+	 */
+	public AdaptateurBufferedImage(BufferedImage bufferedimage) {
+		this.bufferedimage = bufferedimage;
 	}
+
 	@Override
 	public int getHeight() {
 		return bufferedimage.getHeight();
@@ -31,20 +48,18 @@ public class AdaptateurBufferedImage implements IFractalImage {
 
 	@Override
 	public Pixel getPixel(int row, int column) {
-		return ((IFractalImage) bufferedimage).getPixel(row, column); 
+		return ((IFractalImage) bufferedimage).getPixel(row, column);
 	}
 
 	@Override
 	public void setColor(int row, int column, Color color) {
-		 ((IFractalImage) bufferedimage).setColor(column,row,color);
+		((IFractalImage) bufferedimage).setColor(column, row, color);
 	}
 
 	@Override
 	public void saveAs(String path) throws IOException {
-		// TODO Auto-generated method stub
-	}
-
-	public BufferedImage getBufferedimage() {
-		return bufferedimage;
+		String[] splitted = path.split("\\.");
+		String formatName = splitted[splitted.length - 1];
+		ImageIO.write((RenderedImage) ((IFractalImage) bufferedimage), formatName, new File(path));
 	}
 }
