2fee063734a64904da4cbb9d161fabf51fba1d60
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-25 09:22:15 +0200

    Ajout des méthodes de l'implémentation de INextTerm dans MandelbroNextTerm.

--- src/main/java/fr/univartois/butinfo/fractals/sequences/MandelbrotNextTerm.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/MandelbrotNextTerm.java
@@ -3,31 +3,55 @@ package fr.univartois.butinfo.fractals.sequences;
 import fr.univartois.butinfo.fractals.complex.IComplex;
 
 /**
- * La classe {@link MandelbrotNextTerm} correspond à la stratégie (strategy) pour calculer le
- * prochain terme de la suite de Mandelbrot.
+ * La classe {@link MandelbrotNextTerm} correspond à la stratégie (strategy)
+ * pour calculer le prochain terme de la suite de Mandelbrot.
  *
  * @author Sébastien Coze
  *
  * @version 0.1.0
  */
 public class MandelbrotNextTerm implements INextTerm {
-	
+
+	/**
+	 * Premier terme de la suite de Mandelbrot.
+	 */
+	private IComplex firstTerm;
+
+	/**
+	 * Terme courant de la suite.
+	 */
+	private IComplex presentTerm;
+
+	/**
+	 * Nombre complexe z permettant de générer la suite de Mandelbrot.
+	 */
+	private IComplex z;
+
+	/**
+	 * Crée une nouvelle instance de JuliaNextTerm.
+	 * 
+	 * @param z Le nombre complexe z.
+	 */
+	public MandelbrotNextTerm(IComplex z) {
+		setFirstTerm(z.add(z.multiply(z)));
+	}
+
 	@Override
 	public void setFirstTerm(IComplex firstTerme) {
-		// TODO Auto-generated method stub
-		
+		this.firstTerm = firstTerme;
 	}
 
 	@Override
 	public void setPresentTerm(IComplex presentTerm) {
-		// TODO Auto-generated method stub
-		
+		this.presentTerm = presentTerm;
 	}
 
 	@Override
 	public IComplex calculateNextTerm(IComplex lastTerm) {
-		// TODO Auto-generated method stub
-		return null;
+		if (lastTerm == null)
+			return firstTerm;
+		else
+			return z.add(lastTerm.multiply(z.multiply(z)));
 	}
 
 }
