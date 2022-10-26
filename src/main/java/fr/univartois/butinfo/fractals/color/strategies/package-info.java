86dafef985c02139c65d428329bd3602b49f7973
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-26 10:42:27 +0200

    Ajout d'un package qui comporte toutes les stratégies pour les palettes.

--- src/main/java/fr/univartois/butinfo/fractals/color/strategies/package-info.java
+++ src/main/java/fr/univartois/butinfo/fractals/color/strategies/package-info.java
@@ -2,7 +2,7 @@
  * Le paquetage {@code fr.univartois.butinfo.fractals.color.strategies} fournit
  * les stratégie (strategy) des différentes palettes possibles.
  *
- * @author Théo Journée $ Sébastien Coze
+ * @author Théo Journée & Sébastien Coze
  *
  * @version 0.1.0
  */
