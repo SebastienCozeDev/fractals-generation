f0bf76e6f68c7e3a0d1ef8b9fb48c883832f90c6
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-26 10:21:27 +0200

    Documentation de l'interface IPlanPoint

--- src/main/java/fr/univartois/butinfo/fractals/complex/IPlanPoint.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/IPlanPoint.java
@@ -1,5 +1,7 @@
 package fr.univartois.butinfo.fractals.complex;
 
+/*L'interface qui permet d'implementer des methodes pour l'adaptateur */
+
 public interface IPlanPoint {
 	  public double X();
 	  public double Y();
