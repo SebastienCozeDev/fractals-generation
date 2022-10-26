6d7c752473470a66caef2604bcfaf510de3d6ef0
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-26 16:49:39 +0200

    Implementation de la classe SequenceChaoutique

--- src/main/java/fr/univartois/butinfo/fractals/sequences/SequenceChaotique.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/SequenceChaotique.java
@@ -5,7 +5,19 @@ import java.util.Iterator;
 import fr.univartois.butinfo.fractals.complex.IPlanPoint;
 
 public abstract class SequenceChaotique implements Iterator<IPlanPoint>,ISequenceChaotique {
-
+	 private IPlanPoint premier;
+	    private int nbMaxIteration;
+	    
+	    public SequenceChaotique(IPlanPoint premier, int nbMaxIteration) {
+	        this.premier=premier;
+	        this.nbMaxIteration = nbMaxIteration;
+	    }
+	    public Iterator<IPlanPoint> iterator(){
+	        return (Iterator<IPlanPoint>) new SequenceChaotiqueIterator(this, nbMaxIteration, premier);
+	        
+	    }
+	    
+	    public abstract double getNext(IPlanPoint plan);
 }
 
 
