297fc64465400ec7427e178ec06daf8f7560699f
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-26 10:41:16 +0200

    Modification dans la methode ToString

--- src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
@@ -13,6 +13,7 @@ public class Circle {
 	 * rayon du cercle.
 	 */
 	private int r;
+	
 	/**
 	 * position x corne gauche du rectangle.
 	 */
@@ -40,19 +41,13 @@ public class Circle {
 	
 	private IFigure decorated;
 
-	public Circle(int r, int cx, int cy, String stroke, String fill, int strokeWidth) {
-		this.r=r;
-		this.cx = cx;
-		this.cy = cy;
-		this.stroke = stroke;
-		this.fill = fill;
-		this.strokeWidth = strokeWidth;
-	}
+	public Circle(int r, int cx, int cy, String stroke, String fill, int strokeWidth) {}
+	
 	/**
 	 * Methode de chaine de caractere svg.
 	 */
 	public String toString() {
-		return "<svg width=\"200\" height=\"250\" version=\"1.1\"> < circle  cx=" + cx + ", cy=" + cy+",r=" + r
-				+ ", stroke=" + stroke + ", fill=" + fill + ", strokeWidth=" + strokeWidth + decorated+"/> </svg>";
+		return "< circle  cx=" + cx + ", cy=" + cy+",r=" + r
+				+ ", stroke=" + stroke + ", fill=" + fill + ", strokeWidth=" + strokeWidth + decorated+"/>";
 	}
 }
