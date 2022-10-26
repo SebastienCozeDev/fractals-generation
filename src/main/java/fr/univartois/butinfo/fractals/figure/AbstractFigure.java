2d2a209fefbfdac046c6b7366ae16d17f8a03f5c
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-26 13:07:40 +0200

    Ajout fichier, flush, close

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/AbstractFigure.java
@@ -0,0 +1,21 @@
+package fr.univartois.butinfo.fractals.figure;
+
+import java.io.PrintWriter;
+
+public abstract class AbstractFigure {
+	
+	   
+	
+	
+	public static void main(String[] args) throws Exception {
+	    
+	    PrintWriter writer1 = new PrintWriter(System.out);
+	    writer1.write("<svg width=\"100\" height=\"100\">"+
+	    		
+	    		+"</svg>");
+	    
+	    
+	    writer1.flush();
+	    writer1.close();
+}
+}
