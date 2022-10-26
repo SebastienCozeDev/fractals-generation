65726c116333a01d84049bb3bba9babbc6ac1667
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-26 16:54:24 +0200

    Creation de la classe SequenceChaotiqueIterator

---
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/SequenceChaotiqueIterator.java
@@ -0,0 +1,13 @@
+package fr.univartois.butinfo.fractals.sequences;
+
+import java.util.Iterator;
+
+
+import fr.univartois.butinfo.fractals.complex.Complex;
+import fr.univartois.butinfo.fractals.complex.IPlanPoint;
+
+public class SequenceChaotiqueIterator implements Iterator<IPlanPoint> {
+
+
+
+}
