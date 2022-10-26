5e98cf3d452d8794649165bd1a0f996c43f6e573
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-26 15:07:31 +0200

    Ajout methode fichier svg

--- src/main/java/fr/univartois/butinfo/fractals/figure/AbstractFigure.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/AbstractFigure.java
@@ -2,7 +2,7 @@ package fr.univartois.butinfo.fractals.figure;
 
 import java.io.PrintWriter;
 
-public abstract class AbstractFigure {
+public abstract class AbstractFigure implements IFigureComposite {
 	
 	   
 	
@@ -18,4 +18,12 @@ public abstract class AbstractFigure {
 	    writer1.flush();
 	    writer1.close();
 }
+	
+	public String create() {
+		IFigureComposite rectangle = (IFigureComposite) new Rectangle(0, 0, 0, 0, 0, 0, null, null, 0);
+		IFigureComposite circle= (IFigureComposite) new Circle(0, 0, 0, "black", "transparent",0);
+		FigureComposite figureComposite = new FigureComposite();
+		figureComposite.add(rectangle);
+		return ""+rectangle+"";
+	}
 }
