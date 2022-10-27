40e713995e5eaf4219029ac18811b69a074a8580
Theo Journee <theo_journee@ens.univ-artois.fr>
2022-10-27 09:50:43 +0200

    Modification

--- src/main/java/fr/univartois/butinfo/fractals/figure/Figure.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Figure.java
@@ -1,6 +1,5 @@
 package fr.univartois.butinfo.fractals.figure;
 
-
 /**
  * La classe qui englobe toute les figures.
  *
@@ -8,10 +7,37 @@ package fr.univartois.butinfo.fractals.figure;
  *
  * @version 0.1.0
  */
-public class Figure extends AbstractFigure implements IFigureComposite  {
+public class Figure extends AbstractFigure implements IFigureComposite {
+
+	private Rectangle rectangle;
+	private Circle circle;
+	private Ellipse ellipse;
+	private Chemin chemin;
+	private Polyligne polyligne;
 
 	@Override
 	public void figureString(String forme) {
-		System.out.println("toutes les formes"+forme);
-}
+		System.out.println("toutes les formes" + forme);
+	}
+
+	public String Rectangle() {
+		return rectangle.ToString();
+	}
+
+	protected String Circle() {
+		return circle.ToString();
+	}
+
+	protected String Ellipse() {
+		return ellipse.ToString();
+	}
+
+	protected String Polyligne() {
+		return polyligne.ToString();
+	}
+
+	protected String Chemin() {
+		return chemin.ToString();
+	}
+
 }
