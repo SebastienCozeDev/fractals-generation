056f6e0c6e62fccdfa5a88ce4d06cd1a31a48027
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-28 16:19:42 +0200

    Creation de la classe Feigenbaum

---
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/Feigenbaum.java
@@ -0,0 +1,11 @@
+package fr.univartois.butinfo.fractals.sequences;
+
+import fr.univartois.butinfo.fractals.complex.IPlanPoint;
+import fr.univartois.butinfo.fractals.sequences.chaotic.ISequenceChaotique;
+import fr.univartois.butinfo.fractals.sequences.chaotic.SequenceChaotique;
+
+public abstract class Feigenbaum extends SequenceChaotique implements ISequenceChaotique{
+	
+   
+
+}
