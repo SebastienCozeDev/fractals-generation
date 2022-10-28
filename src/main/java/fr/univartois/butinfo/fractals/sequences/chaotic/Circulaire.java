27592029e2a261421e128ab7154e8682985e82f6
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-28 16:42:04 +0200

    Creation de la classe suite Circulaire

---
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/chaotic/Circulaire.java
@@ -0,0 +1,18 @@
+package fr.univartois.butinfo.fractals.sequences.chaotic;
+
+import fr.univartois.butinfo.fractals.complex.IPlanPoint;
+
+public class Circulaire extends SequenceChaotique implements ISequenceChaotique {
+
+	public Circulaire(IPlanPoint premier, int nbMaxIteration) {
+		super(premier, nbMaxIteration);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getNext(IPlanPoint plan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
