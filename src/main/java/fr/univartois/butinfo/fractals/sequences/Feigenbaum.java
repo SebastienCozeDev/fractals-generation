03b3438890295a4764427834a158ebdfee9d74b5
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-28 16:21:01 +0200

    Fin de la classe Feigenbaum

--- src/main/java/fr/univartois/butinfo/fractals/sequences/Feigenbaum.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/Feigenbaum.java
@@ -5,7 +5,15 @@ import fr.univartois.butinfo.fractals.sequences.chaotic.ISequenceChaotique;
 import fr.univartois.butinfo.fractals.sequences.chaotic.SequenceChaotique;
 
 public abstract class Feigenbaum extends SequenceChaotique implements ISequenceChaotique{
-	
-   
+	public Feigenbaum(IPlanPoint premier, int nbMaxIteration) {
+        super(premier, nbMaxIteration);
+ 
+    }
+
+    @Override
+    public double getNext(IPlanPoint premier) {
+        return ((premier.X()*premier.Y())*(1-premier.Y()));
+    }
+
 
 }
