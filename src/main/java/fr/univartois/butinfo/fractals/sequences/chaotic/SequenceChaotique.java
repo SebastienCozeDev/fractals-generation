17263bfa1202e9814623c85ddf9e3922f2186478
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-29 13:45:04 +0200

    Documentation

--- src/main/java/fr/univartois/butinfo/fractals/sequences/chaotic/SequenceChaotique.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/chaotic/SequenceChaotique.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 import fr.univartois.butinfo.fractals.complex.IPlanPoint;
 
+/*Classe representant les suites chaotiques */
+
 public abstract class SequenceChaotique implements Iterable<IPlanPoint>,ISequenceChaotique {
 	 private IPlanPoint premier;
 	    private int nbMaxIteration;
