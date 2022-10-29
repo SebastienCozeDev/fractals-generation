286897f5f86f3aeb49e0d6689f412377b61c1d69
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-29 13:44:38 +0200

    Documentation

--- src/main/java/fr/univartois/butinfo/fractals/sequences/chaotic/Feigenbaum.java
+++ src/main/java/fr/univartois/butinfo/fractals/sequences/chaotic/Feigenbaum.java
@@ -2,6 +2,8 @@ package fr.univartois.butinfo.fractals.sequences.chaotic;
 
 import fr.univartois.butinfo.fractals.complex.IPlanPoint;
 
+/*La classe representant la suite Feigenbaum*/
+
 public class Feigenbaum extends SequenceChaotique implements ISequenceChaotique{
 	public Feigenbaum(IPlanPoint premier, int nbMaxIteration) {
         super(premier, nbMaxIteration);
