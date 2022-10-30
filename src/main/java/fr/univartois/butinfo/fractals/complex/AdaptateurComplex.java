0f9d5035a951cb6fca1e0a788c484b48ffa02a1b
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-10-30 12:18:42 +0100

    Correction de la documentation.

--- src/main/java/fr/univartois/butinfo/fractals/complex/AdaptateurComplex.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/AdaptateurComplex.java
@@ -2,24 +2,38 @@ package fr.univartois.butinfo.fractals.complex;
 
 /*L'adaptateur permettant de calculer des suites de points du plan réel */
 
+/**
+ * La classe {@link AdaptateurComplex} correspond à l'adaptateur permettant de
+ * calculer des suites de points dans le plan réel.
+ * 
+ * @author Shahin Shakuri
+ * 
+ * @version 0.1.0
+ *
+ */
 public class AdaptateurComplex implements IPlanPoint {
 
+	/**
+	 * Le nombre complexe.
+	 */
 	private IComplex complex;
 
+	/**
+	 * Crée une instance de {@link AdaptateurComplex}.
+	 * 
+	 * @param complex Le nombre complexe de l'adaptateur.
+	 */
 	public AdaptateurComplex(IComplex complex) {
-
 		this.complex = complex;
 	}
 
 	@Override
 	public double X() {
-
 		return this.complex.getRealPart();
 	}
 
 	@Override
 	public double Y() {
-
 		return this.complex.getImaginaryPart();
 	}
 
